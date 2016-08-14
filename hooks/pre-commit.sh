c69fbcde591592fe0277479f8d3b6512707b17e3
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-08-13 23:24:27 -0400

    Pre-commit: git stash is horrible. D:
    I just did a ton of work in all the files now I had to do a reset --hard.

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-git stash -q --keep-index --no-gpg-sign
-echo "Stashing working copy."
-echo "Running tests."
 nosetests
 RESULT=$?
-echo "Restoring working copy."
-git stash pop -q
 [ $RESULT -ne 0 ] && exit 1
 exit 0
