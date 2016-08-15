4c69d46cfad652dcf029ea7347defacf6857fddf
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-08-14 23:14:28 -0400

    ContextHandler: Cleaned up transfer test.

--- tests/labsuite/protocol/test_context_handler.py
+++ tests/labsuite/protocol/test_context_handler.py
@@ -7,25 +7,33 @@ class ContextHandlerTest(unittest.TestCase):
     def setUp(self):
         self.protocol = Protocol()
 
+    def assertVolume(self, well, volume):
+        result = self.protocol._context_handler.get_volume(well)
+        print(well, volume, result)
+        self.assertEqual(volume, result)
+
     def test_transfer(self):
         """ Maintain well volumes during transfers. """
         self.protocol.add_container('A1', 'microplate.96')
         self.protocol.add_container('C1', 'tiprack.p200')
         self.protocol.add_instrument('A', 'p200')
         self.protocol.calibrate('A1', x=1, y=2, z=3)
+        self.assertVolume('A1:A2', 0)
         self.protocol.transfer('A1:A1', 'A1:A2', ul=100)
-        self.protocol.transfer('A1:A2', 'A1:A3', ul=80)
-        self.protocol._initialize_context()
-        vol1 = self.protocol._context_handler.get_volume('A1:A2')
-        self.assertEqual(vol1, 0)
+        self.assertVolume('A1:A2', 100)
+        self.protocol.transfer('A1:A2', 'A1:A3', ul=20)
+        self.assertVolume('A1:A3', 20)
+        self.assertVolume('A1:A2', 80)
+
         run = self.protocol.run()
         next(run)  # Yield to set progress.
-        next(run)  # Run first command.
-        vol2 = self.protocol._context_handler.get_volume('A1:A2')
-        self.assertEqual(vol2, 100)
-        next(run)  # Run second command.
-        vol3 = self.protocol._context_handler.get_volume('A1:A3')
-        self.assertEqual(vol3, 80)
+        self.assertVolume('A1:A2', 0)
+        next(run)  # transfer('A1:A1', 'A1:A2', ul=100)
+        self.assertVolume('A1:A2', 100)
+        next(run)  # transfer('A1:A2', 'A1:A3', ul=20)
+        self.assertVolume('A1:A3', 20)
+        self.assertVolume('A1:A2', 80)
+        next(run)  # transfer('A1:A1', 'A1:A4', ul=120)
 
     def test_find_instrument_by_volume(self):
         """ Find instrument by volume. """
